ce7fe915e090c243aa8fef65702319ed862edfc1
Tomo Norman <tomo.l.norman@gmail.com>
2021-06-09 22:24:03 +0900

    Tutorial on breaks and conditional

--- main.go
+++ main.go
@@ -6,112 +6,40 @@ package main
 import "fmt"
 
 func main() {
+	age := 45
 
-	// Variables
+	fmt.Println(age > 50)
+	fmt.Println(age >= 27)
+	fmt.Println(age == 35)
+	fmt.Println(age != 42)
 
-	// Strings
-	// Here we are explicity naming the type
-	// var nameOne string = "Tomo"
-	// // Here Go will infer that the variable is of type string given the assignment
-	// var nameTwo = "Snickers"
-	// // Here we are declaring the variable to be of type string before hand without a value. Later on, when we assign
-	// // the variable must be a string. The default value will be an empty string if we dont give it a value ourselves.
-	// var nameThree string
+	// Simple example of if else statements
+	if age < 30 {
+		fmt.Println("Age is less than 30")
+	} else if age < 40 {
+		fmt.Println("Age is less than 40")
+	} else {
+		fmt.Println("Age is not less than 40")
+	}
 
-	// fmt.Println(nameOne, nameTwo, nameThree)
+	// Usng continue and break
+	names := []string{"Tomo", "Patrick", "Snickers", "Alex", "Yusuke", "Aline"}
 
-	// // We can reassign a variable as long as it is of the same type.
-	// nameOne = "Patrick"
-	// // and we can assign a variable as long as we have declared it before
-	// nameThree = "Same"
+	for index, value := range names {
+		// the continue will break out of the current iteration, but continue with the loop
+		// so it will output fcontinuing at position 1, but not go down to the "value at ...."
+		// rather it will go back to start the for loop.
+		if index == 1 {
+			fmt.Println("Continuing at pos", index)
+			continue
+		}
+		// break will break out of the loop completly, so the below will only go p to
+		// position 2, and break out (the rest of the elements will not fire)
+		if index > 2 {
+			fmt.Println("Breaking at position", index)
+			break
+		}
 
-	// fmt.Println(nameOne, nameTwo, nameThree)
-
-	// // We can use the below shorthand to declare and assign a variable without having to use the var keyword. It can only be done
-	// // when initializing the variable. Go will infer the type. The shorthand ":=" cannot be used outside a function.
-	// nameFour := "Alex"
-	// fmt.Println(nameFour)
-
-	// // Integers (ints)
-
-	// var ageOne int = 25
-	// var ageTwo = 30
-	// ageThree := 40
-
-	// fmt.Println(ageOne, ageTwo, ageThree)
-
-	// // bits and memory -> we can declare an int with a certain number of bits as per the below.
-	// // an 8 bit integer can have the range -128 through 127
-	// var numOne int8 = 25
-	// var numTwo int8 = -128
-
-	// // a uint must be a positive number, which means we can double the positive range of numbers available to us.
-	// // eg uint8 is available up to 255.
-	// var numThree uint8 = 255
-
-	// fmt.Println(numOne, numTwo, numThree)
-
-	// // Floats must be declared as either 32bit or 64bit
-	// var floatOne float32 = 25.98
-
-	// fmt.Println(floatOne)
-
-	// Using fmt and formatting strings
-
-	//  Print
-	// age := 35
-	// name := "Tomo"
-	// // Print does not print a new line
-	// fmt.Print("Hello, ")
-	// // and Println does
-	// fmt.Println("World")
-	// fmt.Println("This is on a new line")
-	// // Printing Variables
-	// fmt.Println("My age is", age, "and my name is", name)
-
-	// // Formatted Strings using Printf.
-	// // We use %v (known as a format specifier) which will output in the default format in the order
-	// // of the arguments given (think sql)
-	// fmt.Printf("My age is %v and my name is %v \n", age, name)
-	// // %q will wrap the variable in quotes. %q will not work with integers. Notice it outputs a hash
-	// // instead of the number.
-	// fmt.Printf("My age is %q and my name is %q \n", age, name)
-	// // %T will give us the type of variable
-	// fmt.Printf("age is of type %T name is of type %T \n", age, name)
-	// // %f will output a float, but we should declare the number of decimal points we want (0.2 for two, 0.1 for 1 etc.)
-	// fmt.Printf("You scored %0.1f points! \n", 22.55)
-	// // Google format specifiers for more details
-
-	// // Sprintf -> save the formatted strings. It does not print, bur rather returns the formatted string so you can store it.
-	// var str = fmt.Sprintf("my age is %v and my name is %v \n", age, name)
-	// fmt.Println("The saved string is", str)
-
-	// Arrays and Slices
-	// You need to tell the array the length (inside the []) and the type afterwards (int) at time of declaration.
-	// Arrays are fixed length, and can only be of one type.
-	var ages [3]int = [3]int{20, 25, 30}
-	//shorthand
-	// var ages = [3]int{20, 25, 30}. You can use len for the length of the array.
-	fmt.Println(ages, len(ages))
-	// You can use the walrus operator for arrays also.
-	names := [4]string{"Tomo", "Patrick", "Sam", "Snickers"}
-	fmt.Println(names, len(names))
-
-	// Slices
-	// Slices use arrays under the hood but can be manipulated (we can append). You just dont declare the length when you create it.
-	var scores = []int{100, 50, 25}
-	scores[2] = 70
-	// The append returns a new slice, so if we want to change our original slice, we need to reassign
-	scores = append(scores, 89)
-	fmt.Println(scores, len(scores))
-
-	// Slice Ranges
-	// We can return a range of anarray from start to end position, inclusive of the first but not the second.
-	rangeOne := names[1:3]
-	// Not declaring the second number will include everything from the first number, to the last index (inclusive).
-	rangeTwo := names[2:]
-	// Not declaring the first number will include everything from position 0 to the second number, not inclusive.
-	rangeThree := names[:3]
-	fmt.Println(rangeOne, rangeTwo, rangeThree)
-
+		fmt.Printf("The value at pos %v is %v\n", index, value)
+	}
 }
